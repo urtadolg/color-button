463808ce2c420359f86793b878bc3256b5d32462
Leandro <leandro.urtado25@gmail.com>
2022-03-11 09:57:44 -0300

    app.test.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import { useState } from "react";
 
+export function formatColorName(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
+
 function App() {
-  const [buttonStyle, setButtonStyle] = useState("red");
-  const [buttonLabel, setButtonLabel] = useState("Change to blue");
+  const [buttonStyle, setButtonStyle] = useState("mediumVioletRed");
+  const [buttonLabel, setButtonLabel] = useState("Change to Midnight Blue");
   const [isBtnDisabled, setIsBtnDisabled] = useState(false);
 
   const onClickHandler = () => {
-    if (buttonStyle == "red") {
-      setButtonStyle("blue");
-      setButtonLabel("Change to red");
+    if (buttonStyle == "mediumVioletRed") {
+      setButtonStyle("midnightBlue");
+      setButtonLabel("Change to Medium Violet Red");
     } else {
-      setButtonStyle("red");
-      setButtonLabel("Change to blue");
+      setButtonStyle("mediumVioletRed");
+      setButtonLabel("Change to Midnight Blue");
     }
   };
 
@@ -25,11 +29,12 @@ function App() {
       <button
         disabled={isBtnDisabled}
         onClick={onClickHandler}
-        style={{ backgroundColor: buttonStyle }}
+        style={{ backgroundColor: isBtnDisabled ? "gray" : buttonStyle }}
       >
         {buttonLabel}
       </button>
-      <input onClick={onCheckedHandler} type="checkbox" />
+      <label htmlFor="disableBtn">Disable button</label>
+      <input id="disableBtn" onClick={onCheckedHandler} type="checkbox" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,26 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
+import App, { formatColorName } from "./App";
 
 test("button has correct initial color", () => {
   render(<App />);
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
 
-  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "mediumVioletRed" });
 
   fireEvent.click(colorButton);
 
-  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "midnightBlue" });
 
-  expect(colorButton).toHaveTextContent("Change to red");
+  expect(colorButton).toHaveTextContent("Change to Medium Violet Red");
 });
 
 test("initial conditions", () => {
   render(<App />);
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
 
   expect(colorButton).toBeEnabled();
 
@@ -27,8 +31,10 @@ test("initial conditions", () => {
 
 test("button disabled after checkbox checked", () => {
   render(<App />);
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  const checkbox = screen.getByRole("checkbox");
+  const colorButton = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
 
   fireEvent.click(checkbox);
 
@@ -37,4 +43,33 @@ test("button disabled after checkbox checked", () => {
   fireEvent.click(checkbox);
 
   expect(colorButton).toBeEnabled();
+});
+
+test("button workflow behaviour", () => {
+  render(<App />);
+  const btn = screen.getByRole("button", { name: "Change to Midnight Blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+
+  fireEvent.click(checkbox);
+  expect(btn).toHaveStyle({ backgroundColor: "gray" });
+  fireEvent.click(checkbox);
+  expect(btn).toHaveStyle({ backgroundColor: "mediumVioletRed" });
+
+  fireEvent.click(btn);
+  fireEvent.click(checkbox);
+  expect(btn).toHaveStyle({ backgroundColor: "gray" });
+  fireEvent.click(checkbox);
+  expect(btn).toHaveStyle({ backgroundColor: "midnightBlue" });
+});
+
+describe("Format Color Name Group", () => {
+  test("Simple color name works", () => {
+    expect(formatColorName("Red")).toBe("Red");
+  });
+  test("Single capital letter works", () => {
+    expect(formatColorName("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("Multiple capital letter works", () => {
+    expect(formatColorName("MediumVioletRed")).toBe("Medium Violet Red");
+  });
 });
